538fbfa137c7ae0468926b29669061b89addbe08
simon <ghdud0503@gmail.com>
2021-03-09 02:47:12 +0900

    second init

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	const name string = "simon"
 	var name2 string = "simon2"
-	// := 자동으로 타입을 추측해서 입력해준다.
+	// := 자동으로 타입을 추측해서 입력해준다...
 	name3 := "simon3"
 	fmt.Println(name)
 	fmt.Println(name2)
